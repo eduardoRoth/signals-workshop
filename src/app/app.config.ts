9ae6600f53f74ebe6dae4f06f0e2f55630c35fba
Eduardo Roth <eduardo@eduardoroth.dev>
2024-05-03 14:06:45 -0600

    feat(): ionic integration, basic app setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,12 @@ import { provideRouter, RouteReuseStrategy } from '@angular/router';
 import { routes } from './app.routes';
 import { IonicRouteStrategy } from '@ionic/angular';
 import { provideIonicAngular } from '@ionic/angular/standalone';
+import {provideHttpClient} from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    provideHttpClient(),
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
   ],
--- src/app/pages/todo/todo.page.ts
+++ src/app/pages/todo/todo.page.ts
@@ -1,15 +1,22 @@
 import {
+  afterNextRender,
   ChangeDetectionStrategy,
   Component,
+  computed,
+  effect,
   inject,
-  OnInit,
+  signal,
 } from '@angular/core';
-import { CommonModule, DatePipe } from '@angular/common';
+import { AsyncPipe, DatePipe, JsonPipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {
+  IonAvatar,
+  IonBadge,
   IonChip,
   IonCol,
   IonContent,
+  IonFab,
+  IonFabButton,
   IonHeader,
   IonIcon,
   IonItem,
@@ -24,7 +31,10 @@ import {
   ToastController,
 } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { squareOutline, checkboxOutline } from 'ionicons/icons';
+import { checkboxOutline, squareOutline, add } from 'ionicons/icons';
+import { HttpClient } from '@angular/common/http';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
 
 interface Task {
   id: number;
@@ -56,53 +66,136 @@ interface Task {
     IonRow,
     IonCol,
     DatePipe,
+    AsyncPipe,
+    IonBadge,
+    JsonPipe,
+    IonFab,
+    IonFabButton,
+    IonAvatar,
   ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TodoPage {
   private readonly toastCtrl = inject(ToastController);
-  // tasks: Array<Task> = [];
-  tasks: Array<Task> = Array(20)
-    .fill(0)
-    .map((c, i) => ({
-      id: i + 1,
-      text: `Activity ${i + 1}`,
-      done: false,
-      created: new Date(),
-    }))
-    .sort((a, b) => (a.done === b.done ? 0 : b.done ? -1 : 1));
+  private readonly http = inject(HttpClient);
+
+  /*
+
+  Signals are Memoized
+
+  First time they're called, they run and generate a value for an input
+  The second time they are called, or nth time, if the signal hasn't received
+  any update, then it's not going to call the function, and just
+  return the previous value.
+
+  Signal knows if it has changed or not.
+
+  "Activity" --> Nothing has changed --> Send back "Activity"
+
+  ... change detection runs for other stuff | "Activity"
+  ... change detection runs for other stuff | "Activity"
+  ... change detection runs for other stuff | "Activity"
+  ... change detection runs for other stuff | "Activity"
+  "Activity" set "Activity 2"
+  ... change detection runs for other stuff | Signal has changed | re-run --> "Activity 2"
+  ... change detection runs for other stuff | "Activity 2"
+  ... change detection runs for other stuff | "Activity 2"
+  ... change detection runs for other stuff | "Activity 2"
+  ... change detection runs for other stuff | "Activity 2"
+
+
+   */
+
+  tasks = signal<Array<Task> | undefined>(undefined);
+  searchText = signal<string | undefined>(undefined);
+
+  filteredTasks = computed(() => {
+    const tasks = this.tasks() ?? [];
+    const search = this.searchText();
+    return tasks
+      .filter((task) =>
+        (search ?? '').length > 0
+          ? task.text.toUpperCase().includes((search ?? '').toUpperCase())
+          : true,
+      )
+      .sort((a, b) => (a.done === b.done ? a.id - b.id : b.done ? 0 : 1));
+  });
+
+  characters = toSignal(
+    this.http
+      .get('https://rickandmortyapi.com/api/character')
+      .pipe(map((response: any) => response.results)),
+  );
 
   constructor() {
+    afterNextRender(() => {
+      const tasks = JSON.parse(window.localStorage.getItem('tasks') ?? '[]');
+      this.tasks.set(tasks);
+      const currentSearch = window.localStorage.getItem('search') ?? '';
+      this.searchText.set(currentSearch);
+    });
+
+    effect(() => {
+      const currentSearch = this.searchText();
+      if (currentSearch) {
+        window.localStorage.setItem('search', currentSearch);
+      }
+      console.log('change in the search', currentSearch);
+    });
+
+    effect(() => {
+      const tasks = this.tasks();
+      console.log('Change in tasks!', tasks);
+      if (tasks) {
+        window.localStorage.setItem('tasks', JSON.stringify(tasks));
+      }
+    });
+
     addIcons({
       squareOutline,
       checkboxOutline,
+      add,
     });
+  }
+
+  async addTodo() {
+    this.tasks.set([
+      ...(this.tasks() ?? []),
+      {
+        id: parseInt(`${Math.random() * 300}`),
+        text: `New activity ${new Date()}`,
+        created: new Date(),
+        done: false,
+      },
+    ]);
   }
 
   async updateTask(task: Task) {
     if (await this.toastCtrl.getTop()) {
       this.toastCtrl.dismiss();
     }
-    task.done = !task.done;
-    task.finished = new Date();
-    const message: string = task.done
+    const done = !task.done;
+    this.tasks.set([
+      ...(this.tasks() ?? []).filter((t) => t.id !== task.id),
+      {
+        ...task,
+        done,
+      },
+    ]);
+
+    const message: string = done
       ? `Task ${task.text} completed`
       : `Task ${task.text} marked as pending`;
     (
       await this.toastCtrl.create({
         message,
         duration: 2000,
-        color: task.done ? 'success' : 'primary',
+        color: done ? 'success' : 'primary',
       })
     ).present();
 
-    this.tasks = this.tasks.sort((a, b) =>
-      a.done === b.done
-        ? a.id - b.id
-        : a.done === b.done
-          ? 0
-          : b.done
-            ? -1
-            : 1,
-    );
+    // this.tasks = this.tasks.sort((a, b) =>
+    //   a.done === b.done ? a.id - b.id : a.done === b.done ? 0 : b.done ? -1 : 1,
+    // );
   }
 }
